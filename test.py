4b625640bb48e1447832080fd39d6843c986d0aa
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-05-10 14:33:17 -0500

    Testing file for XML to LOM mapper.

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
-import json
 import xmltodict
+from model import LOMModel
+
 
 def get_lines(path):
     return ''.join(open(path).readlines())
 
 
 data = get_lines(
-    '/home/leo_mx/PycharmProjects/LompadWeb/temp_files/El_sistema_financiero-IMS_CP - IEEE LOM/imsmanifest_anterior.xml')
-
+    'C:\\Users\\torre\\Documents\\RESPALDO\\PycharmProjects\\LompadWeb\\temp_files\\'
+    'El_sistema_financiero-IMS_CP - IEEE LOM\\imsmanifest_anterior.xml')
 
 
 data_dict = xmltodict.parse(data)
-data_json = json.dumps(data_dict, indent=4)
-print(data_json)
+
+leafs = ['lomes:general', 'lomes:lifeCycle', 'lomes:metaMetadata', 'lomes:technical', 'lomes:educational',
+         'lomes:rights', 'lomes:relation', 'lomes:annotation', 'lomes:classification']
+
+
+def recursive_function(dictionary, profundidad):
+    for key, value in dictionary.items():
+        if isinstance(dictionary[key], dict):
+            if key in leafs:
+                LOMModel.determine_lopad_leaf(dictionary[key], key)
+            recursive_function(dictionary[key], profundidad+1)
+
+
+recursive_function(data_dict, 1)
