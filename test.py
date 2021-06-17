ef51af30d924213f273d22962bd0c5e083a9e885
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-06-17 00:05:29 -0500

    Using lxml to map xml to classes.

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import xmltodict
 from model import LOMModel
 from collections import OrderedDict
 
+from lxml import objectify
+from lxml.etree import fromstring
+from lxml import etree
+
 
 def get_lines(path):
     return ''.join(open(path).readlines())
@@ -9,10 +13,11 @@ def get_lines(path):
 
 data = get_lines(
     'C:\\Users\\torre\\Documents\\RESPALDO\\PycharmProjects\\LompadWeb\\temp_files\\'
-    'El_sistema_financiero-IMS_CP - IEEE LOM\\imsmanifest_anterior.xml')
-
+    'El_sistema_financiero-IMS_CP - IEEE LOM\\imsmanifest_anterior.xml').encode('utf-8')
+parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
 
 data_dict = xmltodict.parse(data)
+
 
 leafs = ['lomes:general', 'lomes:lifeCycle', 'lomes:metaMetadata', 'lomes:technical', 'lomes:educational',
          'lomes:rights', 'lomes:relation', 'lomes:annotation', 'lomes:classification']
@@ -21,9 +26,16 @@ leafs = ['lomes:general', 'lomes:lifeCycle', 'lomes:metaMetadata', 'lomes:techni
 def recursive_function(dictionary):
     for key, value in dictionary.items():
         if isinstance(dictionary[key], dict):
-            if key in leafs:
+            if any(key in leaf for leaf in leafs):
                 LOMModel.determine_lopad_leaf(dictionary[key], key)
             recursive_function(dictionary[key])
 
 
 recursive_function(data_dict)
+
+dispatch = {
+    'lomes:general': 'general_leaf', 'lomes:lifeCycle': 'life_cycle_leaf', 'lomes:metaMetadata': 'meta_metadata_leaf',
+    'lomes:technical': 'technical_leaf', 'lomes:educational': 'educational_leaf',
+    'lomes:rights': 'rights_leaf', 'lomes:relation': 'relation_leaf', 'lomes:annotation': 'annotation_leaf',
+    'lomes:classification': 'classification_leaf'
+}
