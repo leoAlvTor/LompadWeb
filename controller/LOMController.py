9f4a5ecaee532b9319cacf0a6758dba569d34ac0
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-06-24 15:52:42 -0500

    New method for parsing a valid XML string to OrderedDict (Subclass of Dict class).

--- controller/LOMController.py
+++ controller/LOMController.py
@@ -10,6 +10,17 @@ class Controller():
     _leafs = ['lomes:general', 'lomes:lifeCycle', 'lomes:metaMetadata', 'lomes:technical', 'lomes:educational',
          'lomes:rights', 'lomes:relation', 'lomes:annotation', 'lomes:classification']
 
+    def parse_str_to_dict(self, data: str) -> OrderedDict:
+        """
+        Parse a valid xml (string) to Python OrderedDict class (Subclass of Dict class).
+
+        :param data: A valid XML string.
+        :type data str
+
+        :return: An instance of OrderedDict
+        """
+        return xmltodict.parse(data)
+
     def map_recursively(self, dictionary: dict):
         """
         Based on an OrderedDict this method map recursively the Dictionary to Python Class.
