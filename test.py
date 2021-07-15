e6c12e1fe1a94e4d39511d11d76edb1329e28c07
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-07-15 16:10:56 -0500

    Test file IGNORE IT.

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
 
-leo = OrderedDict([('@uniqueElementName', 'typicalLearningTime'), ('duration', OrderedDict([('@uniqueElementName', 'duration'), ('#text', 'P0Y0M0DT1H0M0S')])), ('description', OrderedDict([('string', OrderedDict([('@language', 'es'), ('#text', 'No toma mas de una hora.')]))]))])
-print(leo['description']['string']['#text'])
+leo = OrderedDict([('id', '0'),
+                    ('entry',
+                     OrderedDict([('string',
+                                   OrderedDict([('@language', 'Espanol'),
+                                                ('#text', 'tax1')]))]))])
 
-print('description' in leo.keys() and 'string' in leo['description'].keys() and '#text' in leo['description']['string'])
+print()
